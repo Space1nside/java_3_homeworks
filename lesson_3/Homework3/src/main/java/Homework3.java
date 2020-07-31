1ff1ab769f995c64a8a272b032a7f8582c78d09e
Adilet <jostonfer@gmail.com>
2020-07-31 20:42:26 +0600

    Initial commit

--- lesson_3/Homework3/src/main/java/Homework3.java
+++ lesson_3/Homework3/src/main/java/Homework3.java
@@ -6,33 +6,33 @@ public class Homework3 {
         File file = new File("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\Just_File");
         try {
             //task one
-//            FileInputStream fis = new FileInputStream(file);
-//            byte[] arrForFile = new byte[(int) file.length()];
-//            int x;
-//            while ((x = fis.read(arrForFile)) != -1) {
-//                System.out.println(Arrays.toString(arrForFile));
-//            }
-//            fis.close();
-//            System.out.println();
-//
-//            //task two
-//            ArrayList<InputStream> fileArray = new ArrayList<>();
-//            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\firstFile"));
-//            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\secondFile"));
-//            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\thirdFile"));
-//            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\fouthFile"));
-//            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\fifthFile"));
-//
-//            SequenceInputStream connectedFiles = new SequenceInputStream(Collections.enumeration(fileArray));
-//            FileOutputStream outFilesIntoOne = new FileOutputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\FiveInOneFile");
-//            x = 0;
-//            while ((x = connectedFiles.read()) != -1) {
-//                outFilesIntoOne.write(x);
-//                System.out.print((char) x);
-//            }
-//            connectedFiles.close();
-//            outFilesIntoOne.close();
-//            System.out.println("\n");
+            FileInputStream fis = new FileInputStream(file);
+            byte[] arrForFile = new byte[(int) file.length()];
+            int x;
+            while ((x = fis.read(arrForFile)) != -1) {
+                System.out.println(Arrays.toString(arrForFile));
+            }
+            fis.close();
+            System.out.println();
+
+            //task two
+            ArrayList<InputStream> fileArray = new ArrayList<>();
+            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\firstFile"));
+            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\secondFile"));
+            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\thirdFile"));
+            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\fouthFile"));
+            fileArray.add(new FileInputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\fifthFile"));
+
+            SequenceInputStream connectedFiles = new SequenceInputStream(Collections.enumeration(fileArray));
+            FileOutputStream outFilesIntoOne = new FileOutputStream("C:\\Java\\Java_3\\lessons\\lesson_3\\java_3_homeworks\\lesson_3\\Homework3\\src\\FiveInOneFile");
+            x = 0;
+            while ((x = connectedFiles.read()) != -1) {
+                outFilesIntoOne.write(x);
+                System.out.print((char) x);
+            }
+            connectedFiles.close();
+            outFilesIntoOne.close();
+            System.out.println("\n");
 
             //task three
             Scanner sc = new Scanner(System.in);
