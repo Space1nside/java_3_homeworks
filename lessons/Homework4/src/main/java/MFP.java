311a27b26c93adc9906c9fe7de2232b80de70371
SpaceInside <jostonfer@gmail.com>
2020-08-04 17:46:19 +0600

    Initial commit

--- lessons/Homework4/src/main/java/MFP.java
+++ lessons/Homework4/src/main/java/MFP.java
@@ -1,2 +1,90 @@
 public class MFP {
+    final Object print = new Object();
+    final Object scan = new Object();
+
+    public void scanFile(String fileName, boolean online) {
+        synchronized (scan) {
+            if (online) {
+                System.out.print("Идёт сканирование файла " + fileName + ", пожалуйста подождите");
+                for (int i = 0; i < 6; i++) {
+                    System.out.print(".");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println();
+                System.out.println("Файл " + fileName + " просканирован.");
+            } else {
+                synchronized (print) {
+                    System.out.print("Идёт сканирование файла " + fileName + ", пожалуйста подождите");
+                    for (int i = 0; i < 6; i++) {
+                        System.out.print(".");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println();
+                    System.out.println("Файл " + fileName + " просканирован.");
+                }
+            }
+        }
+    }
+
+    public void printFile(String fileName) {
+        System.out.println();
+        synchronized (print) {
+            System.out.print("Идёт печать файла " + fileName + ", пожалуйста подождите");
+            for (int i = 0; i < 6; i++) {
+                System.out.print(".");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println();
+            System.out.println("Файл " + fileName + " распечатан.");
+        }
+    }
+}
+
+class MainMFP {
+    public static void main(String[] args) {
+        MFP mfp = new MFP();
+        Thread human1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                    mfp.scanFile("Document_1", true);
+            }
+        });
+        human1.start();
+
+        Thread human2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfp.printFile("Document_2");
+            }
+        });
+        human2.start();
+
+        Thread human3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfp.scanFile("Document_3", false);
+            }
+        });
+        human3.start();
+
+        Thread human4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mfp.printFile("Document_3");
+            }
+        });
+        human4.start();
+    }
 }
--- lessons/Homework4/src/main/java/Main.java
+++ lessons/Homework4/src/main/java/Main.java
@@ -1,5 +1,78 @@
 public class Main {
+    private char letter = 'A';
+    private static final int AMOUNT_OF_ABC = 5;
+
     public static void main(String[] args) {
-        
+        Main ABC = new Main();
+        Thread a = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < AMOUNT_OF_ABC; i++) {
+                    ABC.showA();
+                }
+            }
+        });
+
+        Thread b = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < AMOUNT_OF_ABC; i++) {
+                    ABC.showB();
+                }
+            }
+        });
+
+        Thread c = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < AMOUNT_OF_ABC; i++) {
+                    ABC.showC();
+                }
+            }
+        });
+
+        a.start();
+        b.start();
+        c.start();
+    }
+
+    public synchronized void showA() {
+        try {
+            while (letter != 'A') {
+                wait();
+            }
+            System.out.print('A');
+            letter = 'B';
+            notify();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void showB() {
+        try {
+            while (letter != 'B') {
+                wait();
+            }
+            System.out.print('B');
+            letter = 'C';
+            notify();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void showC() {
+        try {
+            while (letter != 'C') {
+                wait();
+            }
+            System.out.print('C' + " ");
+            letter = 'A';
+            notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
+
